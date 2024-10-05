39605f528e31949e8ad70f5b941eb611d2c479d3
HerverLuis <luisantonioherver908@gmail.com>
2024-10-04 23:02:02 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+document.getElementById('registroForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevenir el envío del formulario
+    
+    const matricula = document.getElementById('matricula').value;
+    const mensaje = document.getElementById('mensaje');
+    
+    // Lista de prefijos válidos
+    const prefijosValidos = ['1084','1184','1284','1384','1484','1584','1684','1784','1884', '1984', '2084', '2184', '2284', '2384', '2484'];
+    
+    // Validar que la matrícula tenga 8 caracteres y comience con uno de los prefijos válidos
+    const esValido = prefijosValidos.some(prefijo => matricula.startsWith(prefijo)) && matricula.length === 8;
+
+    if (!esValido) {
+        mensaje.textContent = 'Matrícula no válida.';
+        mensaje.style.color = 'black';
+    } else {
+        mensaje.textContent = 'El estudiante está inscrito.';
+        mensaje.style.color = 'blue';
+    }
+});
+
+   
